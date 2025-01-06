0ba78639057bd49bcdad2481012b4aaee9adfcb1
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-01-06 09:44:18 -0300

    CompatorAllExamples

--- src/application/ComparatorAula.java
+++ src/application/ComparatorAula.java
@@ -19,11 +19,24 @@ public class ComparatorAula {
 		p.add(new Product("Tv", 900.00));
 		p.add(new Product("Notebook", 1200.0));
 		p.add(new Product("Tablet", 450.00));
-		
-		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-
 	
-	p.sort(comp);
+		// ArrowFunction Sintaxe
+		/*Comparator<Product> comp = (p1, p2) ->{
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+		};*/
+		
+		/*Comparator<Product> comp = new Comparator<Product>() {
+
+			@Override
+			public int compare(Product o1, Product o2) {
+				return o1.getName().toUpperCase().compareTo(o2.getName().toUpperCase());
+			}
+		
+		};*/
+		
+		p.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+		
+		
 		
 		/* p.sort(new MyComparator()); */
 		
